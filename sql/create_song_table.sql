4924aee65a84050dfecac496ce1021ad3fc76d2e
JonAWhite <jonathan.jawhite@gmail.com>
2015-11-05 22:40:55 +0000

    Adding sql code for creating a song table.

---
+++ sql/create_song_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE choir_tracker.Songs 
+(
+SongID varchar(4),
+Book varchar(20),
+SongNumber int,
+Name varchar(255),
+Songwriter varchar(255),
+Lyricist varchar(255),
+YearComposed int,
+PRIMARY KEY (SongID)
+);
