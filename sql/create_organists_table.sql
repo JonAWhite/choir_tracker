03d17ab94af4becddeaa016fc7e547b62b5df493
JonAWhite <jonathan.jawhite@gmail.com>
2016-02-04 23:43:58 +0000

    Adding Song History Table, as well as choir directors and organists table.

---
+++ sql/create_organists_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE choir_tracker.Organists
+(
+ID varchar(4),
+FirstName varchar(50),
+LastName varchar(50),
+PRIMARY KEY (ID)
+);
+
--- sql/create_song_history_table.sql
+++ sql/create_song_history_table.sql
@@ -1,9 +1,16 @@
 CREATE TABLE choir_tracker.Song_History
 (
-SongID varchar(4),
+ID varchar(4),
 Date DATE, 
 DirectorID varchar(4), 
 OrganistID varchar(4), 
 EventID varchar(4), 
-PRIMARY KEY (SongID)
+PRIMARY KEY (ID),
+INDEX dir_ind (DirectorID),
+FOREIGN KEY (DirectorID)
+    REFERENCES Choir_Directors(id)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (OrganistID)
+    REFERENCES Organists(id)
+    ON UPDATE CASCADE ON DELETE RESTRICT
 );
