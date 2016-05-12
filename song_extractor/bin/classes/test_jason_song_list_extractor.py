016ec26d9e269be6c6344aa10f5f96305c15b041
JonAWhite <jonathan.jawhite@gmail.com>
2016-05-12 17:40:57 +0000

    reload ctags automatically via git hook

--- song_extractor/bin/classes/test_jason_song_list_extractor.py
+++ song_extractor/bin/classes/test_jason_song_list_extractor.py
@@ -5,7 +5,7 @@ from classes.song_list import SongList
 
 class JasonSongListExtractorTestCase(unittest.TestCase):
     def test_songs_with_apostrophes(self):
-        text = "COB 92 O come to God's altar v2 "
+        text = "COB 92 O come to God's altar v2"
         html = ""
         song_list = JasonSongListExtractor(text, html).extract_song_list()
         print(song_list)
