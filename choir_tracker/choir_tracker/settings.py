18e70d31c9728c2f838cd700a95949ecae4ab891
JonAWhite <jonathan.jawhite@gmail.com>
2016-05-17 16:26:25 +0000

    Adding Database Connection

--- choir_tracker/choir_tracker/settings.py
+++ choir_tracker/choir_tracker/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'choir_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'choir_tracker',
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
